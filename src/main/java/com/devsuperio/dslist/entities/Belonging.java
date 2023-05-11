4cc58bd4c458496f8bce48c6ef79016a00799ca4
dannydagamaisutic@gmail.com <dannydagamaisutic@gmail.com>
2023-05-11 22:16:27 +0100

    Seed database

--- src/main/java/com/devsuperio/dslist/entities/Belonging.java
+++ src/main/java/com/devsuperio/dslist/entities/Belonging.java
@@ -1,2 +1,55 @@
-package com.devsuperio.dslist.entities;public class Belonging {
+package com.devsuperio.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+    private Integer position;
+    @EmbeddedId
+    private BelongingPk id= new BelongingPk();
+    public Belonging(){
+    }
+
+    public Belonging(Integer position, Game game, GameList list) {
+        id.setGame(game);
+        id.setList(list);
+
+        this.position = position;
+
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public BelongingPk getId() {
+        return id;
+    }
+
+    public void setId(BelongingPk id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
